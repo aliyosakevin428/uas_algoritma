4970c2af09c8bd1e825968a153d8192f4f382ef0
Kevin420 <155149448+aliyosakevin428@users.noreply.github.com>
2024-03-27 13:49:05 +0800

    nambah file lagi hehe

---
+++ grade_nilai.php
@@ -0,0 +1,22 @@
+<?php
+// Nilai yang ingin dicek
+$nilai = 111; 
+
+// Untuk Mendapatkan grade nilai
+if ($nilai >= 0 && $nilai <= 40) {
+    $grade = "E";
+} elseif ($nilai >= 41 && $nilai <= 50) {
+    $grade = "D";
+} elseif ($nilai >= 51 && $nilai <= 70) {
+    $grade = "C";
+} elseif ($nilai >= 71 && $nilai <= 85) {
+    $grade = "B";
+} elseif ($nilai >= 86 && $nilai <= 100) {
+    $grade = "A";
+} else {
+    $grade = "Tidak valid"; 
+}
+
+// Menampilkan hasil
+echo "Grade nilai dari $nilai adalah $grade";
+?>
---
+++ panjang_karakter.php
@@ -0,0 +1,10 @@
+<?php
+// Variable Nama Saya
+$nama = "Aliyosa Kevin"; // Ganti "Contoh Nama" dengan nama Anda
+
+// Menghitung jumlah karakter
+$jumlah_karakter = strlen($nama);
+
+// Menampilkan hasil untuk diatas
+echo "Jumlah karakter dari nama saya $nama adalah $jumlah_karakter karakter.";
+?>
