9baa09f86fb924440d53e0cf38f850e784fbaaf4
Kevin420 <155149448+aliyosakevin428@users.noreply.github.com>
2024-03-27 13:31:42 +0800

    nambah file

---
+++ luas_lingkaran.php
@@ -0,0 +1,10 @@
+<?php
+// Menentukan nilai jari-jari
+$jarijari = 5; // Anda bisa mengubah nilai ini sesuai keinginan
+
+// Menghitung luas lingkaran
+$hasil = M_PI * pow($jarijari, 2);
+
+// Menampilkan hasil
+echo "Luas lingkaran dengan jari-jari $jarijari adalah $hasil";
+?>
